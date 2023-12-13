32e677c566e4ad42300bd240f7171bf800a77f10
Vladimir Minkin <vladimir.minkin@gmail.com>
2023-12-13 10:52:10 +0300

    Lesson 4 - finish, todo list with numbers

--- main.js
+++ main.js
@@ -2,15 +2,48 @@ const domOutput = document.getElementById('output');
 const domInput = document.getElementById('input');
 const domButton = document.getElementById('enter');
 
-domButton.onclick = function(event) {
+let numberOfTodos = 0;
+
+// LISTENERS METHODS
+const onCreateTodoButtonClick = function(event) {
   console.log('domButton -> onclick: 0) - check input is not empty');
-  if (domInput.value.length > 0) {
-    console.log('domButton -> onclick: 1) - add todo');
-    domOutput.innerHTML += `<div>${domInput.value}</div>`;
-    console.log('domButton -> onclick: 2) - clear input');
-    domInput.value = '';
-  } else {
-    domInput.style.borderColor = "red"
-    // alert('Wrong input');
+
+  if (checkInputWithText()) {
+    numberOfTodos += 1;
+    renderInputTextInOutput();
   }
+  else renderAlert('Wrong input');
 }
+const onInputTextInput = function() {
+  renderInputHighlightColor("");
+}
+const onTodoDeleteClick = function() {
+  console.log('todoDelete -> onTodoDeleteClick');
+}
+// LISTENERS ENDs
+
+// LOGICAL METHODS
+const checkInputWithText = () => domInput.value.length > 0 && isNaN(domInput.value);
+// LOGICAL ENDs
+
+// RENDER METHODS
+const renderAlert = function(message) {
+  renderInputHighlightColor("red");
+  alert(message);
+}
+const renderInputTextInOutput = function() {
+  console.log('domButton -> onclick: 1) - add todo');
+  domOutput.innerHTML += `
+    <div>
+      ${numberOfTodos}. ${domInput.value} 
+      <button style="background-color:red;" onclick="onTodoDeleteClick">x</button>
+    </div>
+  `;
+  console.log('domButton -> onclick: 2) - clear input');
+  domInput.value = '';
+}
+const renderInputHighlightColor = (color) => domInput.style.backgroundColor = color;
+// RENDER ENDs
+
+domButton.onclick = onCreateTodoButtonClick;
+domInput.oninput = onInputTextInput;
